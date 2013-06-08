a343838d368f7bcba80713baf0a327ede15d184e
Anthony J Bonkoski <ajbonkoski@gmail.com>
2013-06-08 08:09:54 -0400

    added dynamic array type

---
+++ src/common/varray.c
@@ -0,0 +1,59 @@
+#include "varray.h"
+
+#define MIN_ALLOC 16
+
+struct varray
+{
+    uint alloc;     // size of data[] in terms of allocated space
+    uint size;      // size of data[] in terms of used space
+    void **data;
+};
+
+varray_t *varray_create()
+{
+    varray_t *this = calloc(1, sizeof(varray_t));
+    this->alloc = MIN_ALLOC;
+    this->size = 0;
+    this->data = malloc(this->alloc * sizeof(void *));
+    return this;
+}
+
+void varray_add(varray_t *this, void *obj)
+{
+    if(this->size == this->alloc) {
+        this->alloc *= 2;
+        this->data = realloc(this->data, this->alloc);
+    }
+
+    this->data[this->size++] = obj;
+}
+
+void *varray_get(varray_t *this, uint i)
+{
+    ASSERT(0 <= i && i < this->size, "Error: Out of Bounds in varray_get()");
+    return this->data[i];
+}
+
+uint varray_size(varray_t *this)
+{
+    return this->size;
+}
+
+bool varray_iter_next(varray_t *this, int *it, void **val)
+{
+    // reached the end?
+    if(*it >= this->size)
+        return false;
+
+    *val = this->data[*it];
+    (*it)++;
+    return true;
+}
+
+void varray_destroy(varray_t *this)
+{
+    if(this->data != NULL)
+        free(this->data);
+    free(this);
+}
+
---
+++ src/common/varray.h
@@ -0,0 +1,27 @@
+#ifndef VARRAY_H
+#define VARRAY_H
+
+#include "common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct varray varray_t;
+
+varray_t *varray_create();
+void  varray_add(varray_t *this, void *obj);
+void *varray_get(varray_t *this, uint i);
+uint  varray_size(varray_t *this);
+bool  varray_iter_next(varray_t *this, int *it, void **val);
+void  varray_destroy(varray_t *this);
+
+// helper macros
+#define varray_iter(variable, varray) \
+    for(int varray_iter_variable = 0; varray_iter_next((varray), &varray_iter_variable, (void **)&(variable));)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
