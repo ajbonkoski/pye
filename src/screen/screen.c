65b8f14867ec25d1c8bb305d670dc1ca0e47498b
Anthony J Bonkoski <ajbonkoski@gmail.com>
2013-06-09 23:00:11 -0400

    added message box write impl

--- src/screen/screen.c
+++ src/screen/screen.c
@@ -1,12 +1,16 @@
 #include "screen.h"
 
 #define IMPL_TYPE 0x7c870c4b
+#define BLANK ' '
 
 typedef struct
 {
     screen_t *super;
 
     display_t *display;
+    uint mb_y;
+    uint disp_width;
+
     varray_t *buffers;
     void *curbuffer;
 
@@ -48,8 +52,32 @@ static void set_buffer(screen_t *scrn, uint id)
 
 static void write_mb(screen_t *scrn, const char *str)
 {
-    //screen_internal_t *this = cast_this(scrn);
-    ASSERT_UNIMPL();
+    screen_internal_t *this = cast_this(scrn);
+
+    // step 1: start by saving the current cursor position
+    uint x, y;
+    this->display->get_cursor(this->display, &x, &y);
+
+    // step 2: move cursor to the start of the mb line
+    this->display->set_cursor(this->display, 0, this->mb_y);
+
+    // step 3: prepare display string and write it
+    char *buf = malloc(this->disp_width * sizeof(char));
+    bool at_end = false;
+    for(int i = 0; i < this->disp_width; i++) {
+        if(str[i] == '\0')
+            at_end = true;
+        if(!at_end) {
+            buf[i] = str[i];
+        } else {
+            buf[i] = BLANK;
+        }
+    }
+    this->display->write(this->display, buf, this->disp_width);
+    free(buf);
+
+    // step 4: restore cursor position
+    this->display->set_cursor(this->display, x, y);
 }
 
 static void destroy(screen_t *scrn)
@@ -67,6 +95,10 @@ static screen_internal_t *screen_create_internal(screen_t *s, display_t *disp)
     screen_internal_t *this = calloc(1, sizeof(screen_internal_t));
     this->super = s;
     this->display = disp;
+    uint w, h;
+    this->display->get_size(this->display, &w, &h);
+    this->mb_y = h-1;
+    this->disp_width = w;
     this->buffers = varray_create();
     return this;
 }
